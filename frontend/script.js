6b5083f8ea4660106bb8b74536ebaa6d0ab07bea
Attila Talpai <agtalpai@gmail.com>
2022-05-12 08:37:22 +0200

    end of wednesday

--- frontend/script.js
+++ frontend/script.js
@@ -10,6 +10,28 @@ const loadEvent = function () {
 
     const menuButtonElement = document.getElementById("menu-btn");
 
+    const beerSectionComponent = function(title, sub, text, id) {
+        return `
+        <section id="${id}">
+        <h1 class="beerName">${title}</h1>
+        <h2 class="beerCompany">${sub}</h2>
+        <h3 class="beerType">${text}</h3>
+        </section>
+        `
+    }
+
+    const beerAnchorsComponent = function (title, id){
+        return`
+            <a href ="${id}">${title}</a>
+        `
+    }
+
+    const beerNavComponent = function (inner){
+        return `
+            <nav>${inner}</nav>
+        `
+    }
+
     menuButtonElement.addEventListener("click", function (event) {
 
         event.currentTarget.classList.toggle("clicked");
@@ -17,6 +39,27 @@ const loadEvent = function () {
         // console.log(event.currentTarget.closest("#root"));
         event.currentTarget.closest("#root").classList.toggle("menu-opened");
     })
+
+    let beerSections = "";
+    
+    for (const beer of beers.cards) {
+        beerSections += beerSectionComponent(beer.title, beer.sub, beer.text);
+    }
+    rootElement.insertAdjacentHTML("beforeend", beerSections);
+    //console.log(beerSections);
+
+    let beerAnchors = "";
+
+
+    for (const beer of beers.cards) {
+        beerAnchors += beerAnchorsComponent(beer.title)
+    }
+
+    rootElement.insertAdjacentHTML("beforeend", beerNavComponent(beerAnchors))
+
+    const navBarElement = document.getElementById("menu-btn");
+    
+    navBarElement.addEventListener("click", menuButtonClickedEvent)
 
 };
 
